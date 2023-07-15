4300a594922a37dda8b7600b666ee12de19f4c4c
Timhello1 <calkatym@gmail.com>
2023-07-15 20:13:59 +0200

    working2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
 import "./Styles/styles.css";
-import {BrowserRouter} from "react-router-dom"
+import {HashRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
