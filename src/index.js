81408260e139f13598411a58555fcb2d00d5cd91
Timhello1 <calkatym@gmail.com>
2023-07-15 20:44:43 +0200

    hope

--- src/index.js
+++ src/index.js
@@ -3,24 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
 import "./Styles/styles.css";
-<<<<<<< HEAD
+
 import {HashRouter} from "react-router-dom"
-=======
-import {BrowserRouter} from "react-router-dom"
->>>>>>> parent of a09d7dd (Updates)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
+
       <HashRouter>
           <App />
       </HashRouter>
-=======
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
->>>>>>> parent of a09d7dd (Updates)
+
+
   </React.StrictMode>
 );
 
